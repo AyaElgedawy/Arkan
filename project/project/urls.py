a086eb7618c72f39372d96d8af76f1e738093c4e
AyaElgedawy <ayaelgedawy4@gmail.com>
2024-08-16 15:37:47 +0300

    update

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,6 +19,9 @@ from django.urls import path,include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('account.urls')),
