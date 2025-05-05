97525c477c340f581ac24b7a96ed9c914b792698
AyaElgedawy <ayaelgedawy4@gmail.com>
2025-05-05 21:06:14 +0300

    connect productVariant with cart

--- project/cart/models.py
+++ project/cart/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from account.models import CustomUser
-from products.models import Product ,Size ,Color
+from products.models import Product ,Size ,Color ,ProductVariant
 
 class Cart(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)  
@@ -9,6 +9,7 @@ class Cart(models.Model):
     size = models.ForeignKey(Size, on_delete=models.CASCADE, null=True)
     color = models.ForeignKey(Color, on_delete=models.CASCADE, null=True)
     current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)  # Add this line
+    product_variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE,null=True)
     # items = models.PositiveIntegerField()
     # shipping = models.ForeignKey(User, on_delete=models.CASCADE)
 
